d1f9a80a49f298c83e9eee8e13c36700e88a042f
Nastya <nasti.zaitseva@yandex.ru>
2019-05-20 17:56:01 +0300

    Второе задание: создание меню

--- myapplication1/src/main/java/com/example/myapplication1/MainActivity2.java
+++ myapplication1/src/main/java/com/example/myapplication1/MainActivity2.java
@@ -3,6 +3,8 @@ package com.example.myapplication1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -38,5 +40,42 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
                 startActivity(intent);
                 break;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+
+        // TODO Auto-generated method stub
+
+        switch (id){
+            case R.id.page_1:
+                Intent intent=new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.page_2:
+                Intent intent2=new Intent(this, MainActivity1.class);
+                startActivity(intent2);
+                break;
+            case R.id.page_3:
+                Intent intent3=new Intent(this, MainActivity2.class);
+                startActivity(intent3);
+                break;
+            case R.id.page_4:
+                Intent intent4=new Intent(this, MainActivity3.class);
+                startActivity(intent4);
+                break;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return false;
     }
 }
